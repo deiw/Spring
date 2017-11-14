d126f25c3d17c1cdecf3053a34dbb8d9cedacddf
Dawid Majorczyk <majorczyk.dawid@gmail.com>
2017-11-14 11:30:45 +0100

    Added update&remove methods

--- JPA/spring-boot-jpa/src/main/java/pl/majorczyk/springbootjpa/dao/BookDao.java
+++ JPA/spring-boot-jpa/src/main/java/pl/majorczyk/springbootjpa/dao/BookDao.java
@@ -5,4 +5,6 @@ import pl.majorczyk.springbootjpa.model.Book;
 public interface BookDao {
     void save(Book book);
     Book get(Long id);
+    void update(Book book);
+    void remove(Long id);
 }
